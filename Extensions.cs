7a500e9098c6aff8b941278023c7c8a90fdb09c2
SuperJuicer <Sara.Jade@alaskaair.com>
2022-03-22 21:40:22 -0700

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveCards<T>(this IEnumerable<T> firstHalf, IEnumerable<T> secondHalf)
+        {
+            var firstIter = firstHalf.GetEnumerator();
+            var secondIter = secondHalf.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinqFaroShuffle;
+
+Console.WriteLine("Hello, World!");
+
+var deck = from s in Suits()
+           from r in Ranks()
+           select new {Suits = s, Ranks = r };
+
+Console.WriteLine("\nStarting deck:");
+foreach (var card in deck)
+{
+    Console.WriteLine(card);
+}
+
+var topHalf = deck.Take(deck.Count() / 2);
+var bottomHalf = deck.Skip(deck.Count() / 2);
+var shuffledDeck = topHalf.InterleaveCards(bottomHalf);
+
+Console.WriteLine("\nFaro shuffled deck:");
+foreach (var card in shuffledDeck)
+{
+    Console.WriteLine(card);
+}
+
+static IEnumerable<string> Suits()
+{
+    yield return "clubs";
+    yield return "diamonds";
+    yield return "hearts";
+    yield return "spades";
+}
+
+static IEnumerable<string> Ranks()
+{
+    yield return "two";
+    yield return "three";
+    yield return "four";
+    yield return "five";
+    yield return "six";
+    yield return "seven";
+    yield return "eight";
+    yield return "nine";
+    yield return "ten";
+    yield return "jack";
+    yield return "queen";
+    yield return "king";
+    yield return "ace";
+}
